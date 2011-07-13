3595ea042f51689a5be9bdb49d38550bbfcf109f
Teemu Vainio <tim@silvertreksystems.com>
2011-07-13 16:38:16 -0700

    Modern version

---
+++ db/migrate/20110713233133_create_partners.rb
@@ -0,0 +1,16 @@
+class CreatePartners < ActiveRecord::Migration
+  def self.up
+    create_table :partners do |t|
+      t.string :name
+      t.integer :category_id
+      t.integer :partner_type
+      t.string :description
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :partners
+  end
+end
