5731e93823cf367131e1fec9e3a649b18ac99ba7
Teemu Vainio <tim@silvertreksystems.com>
2011-07-14 15:08:17 -0700

    Added model Address, destroyed controller for ContactPerson because it was not necessary

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110714220132) do
+ActiveRecord::Schema.define(:version => 20110714220729) do
+
+  create_table "addresses", :force => true do |t|
+    t.integer  "building_number"
+    t.string   "street"
+    t.string   "city"
+    t.integer  "zip_code"
+    t.string   "state"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "contact_people", :force => true do |t|
     t.string   "name"
