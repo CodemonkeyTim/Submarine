d478ccaa23c2c9320d1069797161980c02e7789b
Teemu Vainio <tim@silvertreksystems.com>
2011-07-15 14:19:30 -0700

    Added the temporary 'item adder form'

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -1,12 +1,12 @@
 class SuppliersController < ApplicationController
   def show
-    
     @supplier = Partner.find(params[:id])
     @job = Job.find_by_job_number(params[:format])
     
-    @open_items = ListItem.find_all_by_job_number_and_partner_id_and_state(params[:format], params[:id], 1)
-    @waiting_items = ListItem.find_all_by_job_number_and_partner_id_and_state(params[:format], params[:id], 2)
-    @closed_items = ListItem.find_all_by_job_number_and_partner_id_and_state(params[:format], params[:id], 3)
+    @yelling_items = ListItem.find_all_by_job_number_and_partner_id_and_state(params[:format], params[:id], 1)
+    @open_items = ListItem.find_all_by_job_number_and_partner_id_and_state(params[:format], params[:id], 2)
+    @waiting_items = ListItem.find_all_by_job_number_and_partner_id_and_state(params[:format], params[:id], 3)
+    @closed_items = ListItem.find_all_by_job_number_and_partner_id_and_state(params[:format], params[:id], 4)
   end
 
   def index
