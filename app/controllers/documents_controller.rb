6d9820cae5599b605f51bb976bf08ce1a43d13b7
Teemu Vainio <tim@silvertreksystems.com>
2011-07-29 12:43:49 -0700

    Something

---
+++ app/controllers/documents_controller.rb
@@ -0,0 +1,11 @@
+class DocumentsController < ApplicationController
+  def new
+    
+  end
+  
+  def edit
+  end
+  
+  def create  
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Submarine::Application.routes.draw do
 
+  get "documents/new"
+
+  get "documents/edit"
+
   match "/history/:id" => "jobs#history"
   
   #assigning a sub
@@ -12,6 +16,7 @@ Submarine::Application.routes.draw do
   resources :subcontractors
   resources :suppliers
   resources :vendors
+  resources :documents
   
   #List item control's routing matches
   match "/modify/:id/:action_id" => "controls#modify"
