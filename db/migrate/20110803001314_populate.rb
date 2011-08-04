6650b876af4a5faeedde31867bf1bbc0a8838e8d
michaelleland <michael@silvertreksystems.com>
2011-08-03 18:54:08 -0700

    added import folder in public

--- db/migrate/20110803001314_populate.rb
+++ db/migrate/20110803001314_populate.rb
@@ -5,6 +5,13 @@ class Populate < ActiveRecord::Migration
          Partner.create :name => line
        end  
      end
+     File.readlines("/home/michaelleland/rails/Submarine/public/jobs.txt").map do |line|
+       while id, name = line.split("\t")
+         job = Job.new
+         job.id = id
+         job.name = name
+       end
+     end
   end
 
   def self.down
