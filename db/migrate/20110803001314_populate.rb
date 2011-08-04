c55e23f42756f91d93031ffdcdcb099b9767d37c
michaelleland <michael@silvertreksystems.com>
2011-08-03 19:17:53 -0700

    migration for jobs

--- db/migrate/20110803001314_populate.rb
+++ db/migrate/20110803001314_populate.rb
@@ -1,17 +1,23 @@
 class Populate < ActiveRecord::Migration
   def self.up
-     File.open("/home/michaelleland/rails/Submarine/public/vendors.txt", 'r') do |pop|
+     File.open("/home/michaelleland/rails/Submarine/public/import/vendors.txt", 'r') do |pop|
        while line = pop.gets
          Partner.create :name => line
        end  
      end
-     File.readlines("/home/michaelleland/rails/Submarine/public/jobs.txt").map do |line|
-       while id, name = line.split("\t")
-         job = Job.new
-         job.id = id
-         job.name = name
-       end
-     end
+      f = File.open("/home/michaelleland/rails/Submarine/public/import/jobs.txt")
+        dataarray = f.read.split("\n")
+      f.close
+        
+        @linesarray =[]
+        dataarray.each {|line| @linesarray << line.split("\t") }
+        
+        @jobs = []
+        @linesarray.each do |line|
+        job = Job.new(:name => line[1], :job_number => line[0])
+        job.save # to database
+        @jobs << job
+        end
   end
 
   def self.down
