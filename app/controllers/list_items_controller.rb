ce31325d309d23de9f121ee0da21734eaef72668
Teemu Vainio <tim@silvertreksystems.com>
2011-09-07 17:06:35 -0700

    Fixed adding items + Fixed redirecting in case of Supplier in both adding items and documents

--- app/controllers/list_items_controller.rb
+++ app/controllers/list_items_controller.rb
@@ -8,10 +8,17 @@ class ListItemsController < ApplicationController
   end
 
   def create
-    @job_id = params[:job_id]
     @asg = Assignment.find_by_job_id_and_parent_id_and_partner_id_and_partner_type(params[:job_id], params[:parent_id], params[:partner_id], params[:partner_type])
     @asg.checklist_items.create(:item_data => params[:item_data], :state => 3, :touched_at => Time.now+16000000000, :sleep_time => 10, :cli_type => params[:repeatable_type])    
     @asg.logs.create(:target_type => "Item", :target_name => "#{params[:item_data]}", :action => "added", :time => get_time, :date => get_date)
+    
+    @page_to_return_to = ""
+    
+    if @asg.partner_type == 1
+      @page_to_return_to = "/subcontractors/#{@asg.partner_id}?job_id=#{@asg.job_id}&parent_id=#{@asg.parent_id}"
+    else
+      @page_to_return_to = "/suppliers/#{@asg.partner_id}?job_id=#{@asg.job_id}&subcontractor_id=#{@asg.parent_id}"
+    end  
   end
 
   def show
