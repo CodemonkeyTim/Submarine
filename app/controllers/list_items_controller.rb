3a78ad6e8e12b74933d28bfca52d0eb146e9ced9
Teemu Vainio <tim@silvertreksystems.com>
2011-12-01 07:41:02 -0800

    List item controller commented

--- app/controllers/list_items_controller.rb
+++ app/controllers/list_items_controller.rb
@@ -1,4 +1,6 @@
 class ListItemsController < ApplicationController
+  
+  #A View Action
   def new
     @name = params[:name]          
     @job_id = params[:job_id]
@@ -7,11 +9,14 @@ class ListItemsController < ApplicationController
     @parent_id = params[:parent_id]
     @partner_type = params[:partner_type]
   end
-
+  
+  #A Background Action
+  #Creates a checklist item with given values
   def create
     @asg = Assignment.find_by_job_id_and_parent_id_and_partner_id_and_partner_type_and_payment_id(params[:job_id], params[:parent_id], params[:partner_id], params[:partner_type], params[:payment_id])
     @payment = Payment.find(params[:payment_id])
     
+    #The new checklist item gets a correct state right from start (overdue/open/waiting)
     if @payment.received?
       if Time.now > @payment.overdue_on
         @asg.checklist_items.create(:item_data => params[:item_data], :state => 1, :sleep_time => 10, :cli_type => params[:repeat_type])
@@ -22,8 +27,10 @@ class ListItemsController < ApplicationController
       @asg.checklist_items.create(:item_data => params[:item_data], :state => 3, :sleep_time => 10, :cli_type => params[:repeat_type])
     end
     
+    #Log mark of item creation
     @asg.logs.create(:target_type => "Item", :target_name => "#{params[:item_data]}", :action => "added", :time => get_time, :date => get_date)
     
+    #The redirect url to the .js view
     @page_to_return_to = ""
     
     if @asg.partner_type == 1
@@ -35,9 +42,9 @@ class ListItemsController < ApplicationController
 
   def show
   end
-
-  def index
-    
+  
+  #A View Action
+  def index  
     #All items categorized by state
     @overdue_items = []
     @open_items = [] 
@@ -46,9 +53,12 @@ class ListItemsController < ApplicationController
     
     @jobs = Job.all
     
+    #loops through all jobs
     @jobs.each do |i|
       @items = i.checklist_items
       
+      #loops through all the checklist items of a job and sorts them by state
+      # into correct arrays
       @items.each do |j|
         if j.state == 1
           @overdue_items.push(j)
